1b8afb2bc9c392f17e36f0536c9436ded943c652
hessamg <hesam_g2002@yahoo.com>
2020-11-02 00:03:01 +0100

    Add first stages of Escape

---
+++ Escape/Source/Escape/Escape.cpp
@@ -0,0 +1,6 @@
+// Developed by Sam Ganjian 2020.
+
+#include "Escape.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Escape, "Escape" );
---
+++ Escape/Source/Escape/WorldPosition.cpp
@@ -0,0 +1,31 @@
+// Developed by Sam Ganjian 2020.
+
+#include "WorldPosition.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UWorldPosition::UWorldPosition()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+// Called when the game starts
+void UWorldPosition::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ObjectName = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s Position in world is: %s"), *GetOwner()->GetName(), *GetOwner()->GetActorTransform().ToString());
+}
+
+// Called every frame
+void UWorldPosition::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
