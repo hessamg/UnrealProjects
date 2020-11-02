965f2c375e695bba914c59fa49aae1db9a3a91e2
hessamg <hesam_g2002@yahoo.com>
2020-11-02 14:56:31 +0100

    fix lightings add readme fix textures

---
+++ Escape/README.md
@@ -0,0 +1,4 @@
+# Escape (Work in Progress)
+### This is a small game. 
+### The Player needs to find a way to escape for the castle.
+
---
+++ Escape/Source/Escape/OpenDoor.cpp
@@ -0,0 +1,32 @@
+// Developed by Sam Ganjian 2020.
+
+#include "OpenDoor.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FRotator CurrentRotation = GetOwner()->GetActorRotation();
+
+	GetOwner()->SetActorRotation(FRotator(0.0f, -180.0f, 0.0f));
+}
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
